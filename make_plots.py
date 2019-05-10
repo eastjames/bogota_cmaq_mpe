Ozone MDA8 calculation and plotting. Various other fixes.

make_plots.py
@@ -84,10 +84,10 @@ def make_scatter( obx, modx, var, fname):
     ax.set_aspect('equal','box')
     #plt.style.context('ggplot')
 
-    fig.savefig('%s.%s' % (fname,frmt), format=frmt, bbox_inches='tight')
+    fig.savefig('../figs/%s.%s' % (fname,frmt), format=frmt, bbox_inches='tight')
     plt.close()
 
-def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
+def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly',o3mda8=None):
     '''
     stats = stats xarray
     avstats: Must be for proper averaging time!
@@ -98,7 +98,7 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
     '''
     avtime = avtime.lower()
     metric = metric.upper()
-    if avtime not in ('hourly', '24h'):
+    if avtime not in ('hourly', '24h', 'mda8'):
         raise ValueError("avtime must be 'hourly', '24h', or 'mda8', %s was given" % \
                          (avtime))
 
@@ -113,29 +113,40 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
     fig, ax = plt.subplots(1)
     
     colors = colorlist()
-    fs=16 #fontsize
+    fs=10 #fontsize
 
     for i, site in enumerate(stats.site):
         # get mean obs if it's not empty
         if not np.isnan(obx[var][:,i]).all():
 #            x = np.mean(obx[var][:,i])
-            x = np.nanmax(obx[var][:,i])
+            if var == 'O3' and metric == 'mda8':
+                if o3mda8 == None:
+                    raise ValueError('Argument o3mda8 must be specified for'+\
+                                      'ozone MDA8 plotting. No argument was given.')
+                x = np.nanmax(o3mda8[:,i])
+            else:
+                x = np.nanmax(obx[var][:,i])
             # get nme value
             if avtime in ('24h', 'mda8'):
                 avtime = '24h/mda8'
             y = stats[metric].loc[site.values,var,avtime]
             if not np.isnan(y):
                 ax.scatter( x, y, marker='o', c=colors[i], edgecolor='black',\
-                            s=250, lw = 1, label=site.values)
+                            s=150, lw = 1, label=site.values)
                 
     criteria = 0
     goal = 0
     ymin, ymax = ax.get_ylim()
-    
+
     # Set max x val for plotting mean of all sites
-    x = avstats[var][6]
-        
+    if var == 'O3':
+        x = np.nanmax(o3mda8)
+    else:
+        x = avstats[var][6]
+
     # Set criteria & goal lines, y lims if applicable, y val for mean of all sites
+    del(goal)
+    del(criteria)
     if metric == 'NME':
         y = avstats[var][1]
         ymin = 0
@@ -161,7 +172,7 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
         print('MFE y = %f' % y)
         ymin = 0
         ymax = 200
-        if var == 'PM10' or 'PM25':
+        if var in ('PM10', 'PM25'):
             criteria = 75
             goal = 50
     elif metric == 'MFB':
@@ -169,7 +180,7 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
         print('MFB y = %f' % y)
         ymin = -200
         ymax = 200
-        if var == 'PM10' or 'PM25':
+        if var in ('PM10','PM25'):
             criteria = 60
             goal = 30
     elif metric == 'R2':
@@ -190,6 +201,7 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
     ax.annotate('All', (x, y), fontsize=fs)
     xmin, xmax = ax.get_xlim()
     lw = 1
+
     if var in ('PM25', 'O3'):
         ax.plot([xmin, xmax],[criteria, criteria],'k-', linewidth=lw, \
                 label='criteria')
@@ -199,16 +211,21 @@ def make_stat_plots(stats,avstats,obx,metric,var,avtime='hourly'):
             ax.plot([xmin, xmax],[-goal, -goal],'k--', linewidth=lw)
     else:
         ax.plot([xmin, xmax],[0,0],'k-', linewidth=lw)
+
     ax.set(xlabel='max obs (%s)'%(units), ylabel='%s (%%)'%(metric), \
            title=obx[var].name, ylim=[ymin,ymax], xlim=[xmin,xmax] )
+
     ax.legend(loc='center left', bbox_to_anchor=(1.01,0.5),\
               frameon=False, fontsize=fs)
+
     ax.title.set_fontsize(fs)
     ax.xaxis.label.set_fontsize(fs)
     ax.yaxis.label.set_fontsize(fs)
     ax.tick_params(labelsize=fs)
-    fig.savefig('%s.%s' % (fname,frmt), format=frmt, bbox_inches='tight')
+    fig.savefig('../figs/%s.%s' % (fname,frmt), format=frmt, bbox_inches='tight')
     plt.close()
+    del(goal)
+    del(criteria)
 
 
 if __name__ == '__main__':
@@ -230,7 +247,7 @@ if __name__ == '__main__':
     avstats24 = mpe.stats_all_sites(obx24, modx24)
     make_scatter(obx,modx,'PM25', 'pm25scatter')
     make_scatter(obx24, modx24, 'PM25', 'pm25scatter24hr')
-    make_stat_plots(stats,avstats24,obx24,'MFE','PM25',avtime='24h')
+    m, o3mda8, o3mda8ake_stat_plots(stats,avstats24,obx24,'MFE','PM25',avtime='24h')
     make_stat_plots(stats,avstats24,obx24,'MFB','PM25',avtime='24h')
     make_stat_plots(stats,avstats,obx,'MFE','PM25',avtime='hourly')
     make_stat_plots(stats,avstats,obx,'MFB','PM25',avtime='hourly')

mpe_stats.py
@@ -24,7 +24,7 @@ def get_obs(season=None, avtime=None):
     Add path to file
     '''
  
-    prefix = '../../observations/ground/' # Bezier
+    prefix = '../../../observations/ground/' # Bezier
 #    prefix = '/ncsu/volume1/fgarcia4/Bogota/observations/ground/' #Henry2
     #prefix = 'obs/' # James Macbook
     if season and avtime:
@@ -202,7 +202,7 @@ def pair_data(obs, modx, metx):
     return obx, modx, metx
 
 
-def calc_mda8(obx, modx):
+def calc_mda8(d):
     '''
     Calculate mda8 for O3
     observation and model dataset
@@ -212,8 +212,37 @@ def calc_mda8(obx, modx):
     
     # time adjustment so mda8 value references 1st hour in 
     # the rolling average
+    times_np = d.O3.time.values.copy()
+    times_pd = pd.to_datetime(times_np) - pd.Timedelta('7h')
+
+    o3a8 = d.O3.rolling(time=8, min_periods=6).mean()
+    o3a8 = o3a8.assign_coords(time=times_pd)
+    o3_mda8 = o3a8.resample(time='D').max(dim='time')
+    o3_mda8 = o3_mda8[1::] # Remove extra 1st max val b/c time shift
+
+    o3_mda8 = o3_mda8.assign_attrs({'units':'ppb'})
+    
+    # Try to assign sitenames. Will only work if observation dataset used.
+    try:
+        o3_mda8 = o3_mda8.assign_attrs({'SITENAMES':d.SITENAMES})
+    except:
+        pass
+
+    return o3_mda8
+
+'''
+def calc_mda8(obx, modx):
+    ''
+    #Calculate mda8 for O3
+    #observation and model dataset
+    #borrowed from http://danielrothenberg.com/\
+    #gcpy/examples/timeseries/calc_mda8_timeseries_xarray.html
+    ''
+    
+    # time adjustment so mda8 value references 1st hour in 
+    # the rolling average
     times_np = modx.O3.time.values.copy()
-    times_pd = pd.to_datetime(times_np) - pd.Timedelta('8h')
+    times_pd = pd.to_datetime(times_np) - pd.Timedelta('7h')
 
     o3a8mod = modx.O3.rolling(time=8, min_periods=6).mean()
     o3a8mod = o3a8mod.assign_coords(time=times_pd)
@@ -230,7 +259,7 @@ def calc_mda8(obx, modx):
     o3_mda8_ob = o3_mda8_ob.assign_attrs({'SITENAMES':obx.SITENAMES})
 
     return o3_mda8_ob, o3_mda8_mod
-
+'''
 
 def calc_stats(obx, modx):
 
@@ -248,11 +277,12 @@ def calc_stats(obx, modx):
 
     #calc mda8 for ozone only
 #    print(modx.time)
-    o3_mda8_ob, o3_mda8_mod = calc_mda8(obx, modx)
+    o3_mda8_ob = calc_mda8(obx)
+    o3_mda8_mod = calc_mda8(modx)
 #    print(modx.time)
     
     mda8obm = ma.masked_invalid(o3_mda8_ob.values) 
-    mda8modm = ma.array(o3_mda8_mod.values, mask=mda8obm)
+    mda8modm = ma.array(o3_mda8_mod.values, mask=mda8obm.mask)
 
     # do stat calcs at sites
     for i in range(len(sites)):
@@ -317,6 +347,7 @@ def stats_all_sites( obx, modx ):
         arrm = np.concatenate(modx[var].values)
         mask = np.isnan(arro)
         arro, arrm = ma.array(arro, mask=mask), ma.array(arrm, mask=mask)
+        #if not arro.count() == 0: 
         nme = calc_NME( arrm, arro )
         nmb = calc_NMB( arrm, arro )
         mfe = calc_MFE( arrm, arro )
@@ -337,7 +368,8 @@ def plot_scatter( obx, modx ):
         mp.make_scatter(obx, modx, var, fname)
         # 24 hour avg
         if var == 'O3':
-            o3_mda8_ob, o3_mda8_mod = calc_mda8(obx, modx)
+            o3_mda8_ob = calc_mda8(obx)
+            o3_mda8_mod = calc_mda8(modx)
             fname24 = 'O3_scatter_mda8'
             mp.make_scatter(o3_mda8_ob, o3_mda8_mod, var, fname24)
         else:
@@ -354,6 +386,7 @@ def plot_stats( obx, modx ):
     obx24, modx24 = make_24h_avg(obx, modx)
     avstats = stats_all_sites( obx, modx )
     avstats24 = stats_all_sites( obx24, modx24)
+    o3mda8 = calc_mda8(obx)
     for var in stats.species.values:
 #        for metric in ['NME', 'NMB', 'R2']:
         for metric in ['NME', 'NMB','MFB','MFE','R2']:
@@ -366,7 +399,7 @@ def plot_stats( obx, modx ):
             if var=='O3':
                 #stuff
                 if not np.isnan( stats[metric].loc[:,var,avtime] ).all():
-                    mp.make_stat_plots(stats, avstats24, obx24, metric, var, 'mda8')               
+                    mp.make_stat_plots(stats, avstats24, obx, metric, var, 'mda8', o3mda8)               
             else:
                 if not np.isnan( stats[metric].loc[:,var,avtime] ).all():
                     mp.make_stat_plots(stats, avstats24, obx24, metric, var, '24h')