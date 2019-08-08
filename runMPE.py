05d95cae34645d70db3179b46827fcd532d3697c
eastjames <jdeast2@ncsu.edu>
2019-08-08 15:53:21 -0400

    Time coords for gridded datasets

--- runMPE.py
+++ runMPE.py
@@ -19,16 +19,21 @@ os.system('mkdir -p ../figs')
 #mfiles = glob.glob('METCRO2D_WRFd04v4n_2014-01-0[123]')
 
 # Bezier
-#cfiles = glob.glob('../../../data_out/bc2014v3/d04/2014-0[123]-??/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_20140[123]??.nc')
-#afiles = glob.glob('../../../data_out/bc2014v3/d04/2014-0[123]-??/CCTM_D502a_Linux3_x86_64intel.AERODIAM.BOGOTA_20140[123]??.nc')
-#mfiles = glob.glob('../../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-0[123]-??')
-cfiles = glob.glob('../../../data_out/bc2014v3/d04/2014-1?-??/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_20141???.nc')
-afiles = glob.glob('../../../data_out/bc2014v3/d04/2014-1?-??/CCTM_D502a_Linux3_x86_64intel.AERODIAM.BOGOTA_20141???.nc')
-mfiles = glob.glob('../../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-1?-??')
+cfiles = glob.glob('../../../data_out/bc2014v4/d04/2014-0[123]-??/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_20140[123]??.nc')
+afiles = glob.glob('../../../data_out/bc2014v4/d04/2014-0[123]-??/CCTM_D502a_Linux3_x86_64intel.AERODIAM.BOGOTA_20140[123]??.nc')
+mfiles = glob.glob('../../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-0[123]-??')
+season = 'JFM'
+
+#cfiles = glob.glob('../../../data_out/bc2014v3/d04/2014-1?-??/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_20141???.nc')
+#afiles = glob.glob('../../../data_out/bc2014v3/d04/2014-1?-??/CCTM_D502a_Linux3_x86_64intel.AERODIAM.BOGOTA_20141???.nc')
+#mfiles = glob.glob('../../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-1?-??')
+#season = 'OND'
+
 try:
     mfiles.remove('../../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-12-31')
 except:
     pass
+
 #cfiles = glob.glob('../../data_out/bc2014v3/d04/2014-0[123]-??/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_20140[123]??.nc')
 #cfiles = glob.glob('../../data_out/bc2014v3/d04/2014-01-0[1234]/CCTM_D502a_Linux3_x86_64intel.ACONC.BOGOTA_2014010[1234].nc')
 #mfiles = glob.glob('../../data_in/mcip/v4n/d04/METCRO2D_WRFd04v4n_2014-01-0[1234]')
@@ -42,14 +47,10 @@ afiles.sort()
 
 # load files
 fc = mpe.get_cmaq(cfiles, afiles)
-fo = mpe.get_obs(season='OND')
+fo = mpe.get_obs(season=season)
 fm = mpe.get_met(mfiles)
 obx, modx, metx = mpe.pair_data(fo, fc, fm)
-#print(modx.time)
-print(obx.O3)
-print(modx.O3)
 stats = mpe.calc_stats(obx, modx)
-#print(modx.time)
 
 print('done!')
 #print(stats)
